7446ed90eb720d7fb89e5aa381f4b31bb504a3ce
B33th0v3n960 <zhousun06@gmail.com>
2023-07-15 18:12:17 +0100

    Update teh nvim-tree keymap settings from deprecated settings

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -1,69 +1,69 @@
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
-  return
+	return
 end
 
-local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
-if not config_status_ok then
-  return
+local function my_on_attach(bufnr)
+	local api = require("nvim-tree.api")
+	local function opts(desc)
+		return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+	end
+
+	api.config.mappings.default_on_attach(bufnr)
+	vim.keymap.set("n", "l", api.node.open.edit, opts("Close"))
+	vim.keymap.set("n", "<CR>", api.node.open.edit, opts("Close"))
+	vim.keymap.set("n", "o", api.node.open.edit, opts("Close"))
+	vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Close"))
 end
 
-local tree_cb = nvim_tree_config.nvim_tree_callback
+nvim_tree.setup({
+	on_attach = my_on_attach,
+	sort_by = "extension",
 
-nvim_tree.setup {
-  sort_by = 'extension',
-  update_focused_file = {
-    enable = true,
-    update_cwd = true,
-  },
-  renderer = {
-    root_folder_modifier = ":t",
-    icons = {
-      glyphs = {
-        default = "",
-        symlink = "",
-        folder = {
-          arrow_open = "",
-          arrow_closed = "",
-          default = "",
-          open = "",
-          empty = "",
-          empty_open = "",
-          symlink = "",
-          symlink_open = "",
-        },
-        git = {
-          unstaged = "",
-          staged = "S",
-          unmerged = "",
-          renamed = "➜",
-          untracked = "U",
-          deleted = "",
-          ignored = "◌",
-        },
-      },
-    },
-  },
-  diagnostics = {
-    enable = true,
-    show_on_dirs = true,
-    icons = {
-      hint = "",
-      info = "",
-      warning = "",
-      error = "",
-    },
-  },
-  view = {
-    width = 30,
-    side = "left",
-    mappings = {
-      list = {
-        { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-        { key = "h", cb = tree_cb "close_node" },
-        { key = "v", cb = tree_cb "vsplit" },
-      },
-    },
-  },
-}
-
+	update_focused_file = {
+		enable = true,
+		update_cwd = true,
+	},
+	renderer = {
+		root_folder_modifier = ":t",
+		icons = {
+			glyphs = {
+				default = "",
+				symlink = "",
+				folder = {
+					arrow_open = "",
+					arrow_closed = "",
+					default = "",
+					open = "",
+					empty = "",
+					empty_open = "",
+					symlink = "",
+					symlink_open = "",
+				},
+				git = {
+					unstaged = "",
+					staged = "S",
+					unmerged = "",
+					renamed = "➜",
+					untracked = "U",
+					deleted = "",
+					ignored = "◌",
+				},
+			},
+		},
+	},
+	diagnostics = {
+		enable = true,
+		show_on_dirs = true,
+		icons = {
+			hint = "",
+			info = "",
+			warning = "",
+			error = "",
+		},
+	},
+	view = {
+		width = 30,
+		side = "left",
+	},
+})
