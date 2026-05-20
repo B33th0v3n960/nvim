c5d9b8849d25fa32a3f0b477badd6fa0c178fba5
B33th0v3n960 <zhousun06@gmail.com>
2026-05-20 22:06:57 +0100

    feat: added border colors to telescope

--- lua/config/plugins/theme.lua
+++ lua/config/plugins/theme.lua
@@ -3,6 +3,14 @@ return {
     "shaunsingh/nord.nvim",
     config = function()
       vim.cmd.colorscheme("nord")
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        callback = function()
+          vim.api.nvim_set_hl(0, "TelescopeBorder", { fg = "#81a1c1" })
+          vim.api.nvim_set_hl(0, "TelescopePromptBorder", { fg = "#d08770" })
+          vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { fg = "#81a1c1" })
+          vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { fg = "#81a1c1" })
+        end,
+      })
     end,
   },
 }
